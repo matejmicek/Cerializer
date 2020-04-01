af3bcd52f13924fb4bfb427f3aec04da18d70d78
Matej Micek <you@example.com>
2020-04-01 09:24:43 +0200

    Added de/serialization feature for fixed schema

--- projekt.pyx
+++ projekt.pyx
@@ -1,14 +1,49 @@
-cpdef serialize(int a, char b):
-    serialized = open('serialized', 'wb')
-    cdef char n[4]
-    n[0] = a
-    n[1] = b
-    serialized.write(n)
-    serialized.close()
+from libc.stdio cimport fopen, fclose, FILE, EOF, fseek, SEEK_END, SEEK_SET, fwrite
+from libc.stdio cimport ftell, fgetc, fgets, getc, gets, feof, fread, getline
+from libc.string cimport strlen, memcpy, strcpy, strtok, strchr, strncpy
+import os
 
-    input_file = open('serialized', 'rb')
-    cdef char p[4]
-    print(input_file.read(1))
 
-    print(p[0])
-    print(p[1])
+
+
+cpdef serialize_two_chars(char filename[], int a, char b):
+    # serialization
+    cdef char printable_bytes[2]
+    cdef FILE *serialized
+    serialized = fopen(filename, 'wb')
+    printable_bytes[0] = a
+    printable_bytes[1] = b
+    fwrite(printable_bytes, 1, 2, serialized)
+    fclose(serialized)
+
+
+
+cpdef int[] deserialize_two_chars(char filename[]):
+    # deserialization
+    cdef FILE *serialized
+    serialized = fopen(filename, 'rb')
+    cdef char chars[2]
+    fread(chars, 1, 2, serialized)
+    return chars
+
+
+
+cpdef serialize_short_int(char filename[], short int a):
+    cdef FILE *serialized
+    serialized = fopen(filename, 'wb')
+    cdef char printable_bytes[2]
+    printable_bytes[0] = (a >> 8) & 0xFF
+    printable_bytes[1] = a & 0xFF
+    fwrite(printable_bytes, 1, 2, serialized)
+    fclose(serialized)
+
+
+cpdef int deserialize_short_int(char filename[]):
+    cdef FILE *serialized
+    serialized = fopen(filename, 'rb')
+    cdef int deserialized_int
+    cdef char file_bytes[2]
+    fread(file_bytes, 1, 2, serialized)
+    fclose(serialized)
+    deserialized_int = (file_bytes[0] << 8) ^ file_bytes[1]
+    return deserialized_int
