1affa7522aeb65d62bee3b694dc80f5196a2a7b3
Matej Micek <you@example.com>
2020-05-04 13:21:37 +0200

    Integrated Fast Avro methods for serialization of basic types

--- examples/avro_exaple.py
+++ examples/avro_exaple.py
@@ -1,17 +1,17 @@
 import avro.schema
 import avro.datafile
 import avro.io
-
+import pprint
 
 
 schema = avro.schema.parse(open('../schemata/user_schema.avsc', 'rb').read())
 
 writer = avro.datafile.DataFileWriter(open('result', 'wb'), avro.io.DatumWriter(), schema)
-writer.append({'name': 'Alyssa', 'favorite_number': 256})
-writer.append({'name': 'Ben', 'favorite_number': 7, 'favorite_color': 'red'})
+writer.append({'name': 'matej'})
+
 writer.close()
 
 reader = avro.datafile.DataFileReader(open('result', 'rb'), avro.io.DatumReader())
 for user in reader:
-	print(user)
+	print(type(user['username']))
 reader.close()
