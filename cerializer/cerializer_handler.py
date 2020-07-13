f6a8eb65f2fdefe7a8cf3f91a95cd0343d1ed8fd
Matej Micek <matej.micek@quantlane.com>
2020-07-13 12:48:32 +0200

    String compilation and minor adjustments

--- cerializer/cerializer_handler.py
+++ cerializer/cerializer_handler.py
@@ -4,7 +4,7 @@ import jinja2
 
 import cerializer.schema_handler
 import constants.constants
-
+import cerializer.compiler
 
 
 '''
@@ -13,12 +13,13 @@ The user should only iteract with the update_schemata method.
 '''
 
 
-def render_code_for_schema(rendered_filename, schema):
+def render_code(schema):
     '''
     Renders code for a given schema into a .pyx file.
     '''
+    # TODO path needs to be fixed - failing tests
     env = jinja2.Environment(
-        loader = jinja2.FileSystemLoader(searchpath = 'cerializer/templates')
+        loader = jinja2.FileSystemLoader(searchpath = '../templates')
     )
     env.globals['env'] = env
     location = 'data'
@@ -28,7 +29,6 @@ def render_code_for_schema(rendered_filename, schema):
         location = location,
         jinja_env = env
     )
-    deserialization_code = code_generator.get_deserialization_code(schema, location, env)
     cdefs = '\n'.join(code_generator.cdefs)
 
     template = env.get_template('template.jinja2')
@@ -36,34 +36,35 @@ def render_code_for_schema(rendered_filename, schema):
         location = location,
         cdefs = cdefs,
         buffer_name = code_generator.buffer_name,
-        serialization_code = serialization_code,
-        deserialization_code = deserialization_code
+        serialization_code = serialization_code
     )
-    output = open(rendered_filename, 'w')
-    output.write(rendered_template)
-    output.close()
+    return rendered_template
 
 
-def update_cerializer(schema_roots):
-    '''
-    Generates code for all schemata in all schema roots and then compiles it.
-    '''
-    code_base_path = 'cerializer/cerializer_base'
-    try:
-        os.mkdir(os.path.join(code_base_path))
-    except OSError:
-        pass
-    for schema_root in schema_roots:
-        schema_root = os.fsencode(schema_root)
-        for namespace in os.listdir(schema_root):
-            for schema_name in os.listdir(os.path.join(schema_root, namespace)):
-                for version in os.listdir(os.path.join(schema_root, namespace, schema_name)):
-                    schema_path = os.path.join(schema_root, namespace, schema_name, version, b'schema.yaml')
-                    filename = f'{schema_name.decode()}_{version.decode()}.pyx'
-                    code_path = os.path.join(code_base_path, filename)
-                    schema = cerializer.schema_handler.parse_schema_from_file(schema_path.decode())
-                    render_code_for_schema(code_path, schema = schema)
-    os.system(f'python setup.py build_ext --inplace')
+
+def get_compiled_code(schema):
+    code = render_code(schema)
+    return cerializer.compiler.compile(code)
 
 
-update_cerializer(['cerializer/tests/schemata'])
+class Cerializer:
+
+    def __init__(self, schema_roots):
+        self.schema_roots = schema_roots
+        self.code = {}
+        self.update_code()
+
+
+    def update_code(self):
+        '''
+        Generates code for all schemata in all schema roots and then compiles it.
+        '''
+        for schema_root in self.schema_roots:
+            schema_root = os.fsencode(schema_root)
+            for namespace in os.listdir(schema_root):
+                for schema_name in os.listdir(os.path.join(schema_root, namespace)):
+                    for version in os.listdir(os.path.join(schema_root, namespace, schema_name)):
+                        schema_path = os.path.join(schema_root, namespace, schema_name, version, b'schema.yaml')
+                        schema = cerializer.schema_handler.parse_schema_from_file(schema_path.decode())
+                        schema_identifier = f'{schema_name.decode()}_{version.decode()}'
+                        self.code[schema_identifier] = get_compiled_code(schema = schema)
--- cerializer/tests/test_cerializer_compatibility.py
+++ cerializer/tests/test_cerializer_compatibility.py
@@ -1,13 +1,12 @@
-import importlib
 import io
 
-import cython
 import fastavro
 import pytest
 import yaml
 
 import constants.constants
 import cerializer.compiler
+import cerializer.cerializer_handler
 
 
 @pytest.mark.parametrize(
@@ -18,29 +17,12 @@ def test_serialization_compatibility(schema_name, schema_version):
 	path = f'schemata/messaging/{schema_name}/{schema_version}/'
 	data = yaml.unsafe_load(open(path + 'example.yaml'))
 	SCHEMA_FAVRO = yaml.load(open(path + 'schema.yaml'), Loader = yaml.Loader)
-	output_fastavro = io.BytesIO() 
-	fastavro.schemaless_writer(output_fastavro, SCHEMA_FAVRO, data)
-	c = importlib.import_module(f'{schema_name}_{schema_version}', package = 'cerializer')
-	output_cerializer = io.BytesIO()
-	c.serialize(data, output_cerializer)
-	assert output_cerializer.getvalue() != io.BytesIO().getvalue()
-	assert output_cerializer.getvalue() == output_fastavro.getvalue()
-
-
-
-@pytest.mark.parametrize(
-	'schema_name,schema_version',
-	constants.constants.SCHEMATA
-)
-def test_string_compiling(schema_name, schema_version):
-	path = f'schemata/messaging/{schema_name}/{schema_version}/'
-	data = yaml.unsafe_load(open(path + 'example.yaml'))
-	output = io.BytesIO()
-	SCHEMA_FAVRO = yaml.load(open(path + 'schema.yaml'), Loader = yaml.Loader)
 	output_fastavro = io.BytesIO()
 	fastavro.schemaless_writer(output_fastavro, SCHEMA_FAVRO, data)
-	with open(f'../cerializer_base/{schema_name + "_" + str(schema_version)}.pyx') as f:
-		code = f.read()
-	serialize = cerializer.compiler.compile(code)['serialize']
-	serialize(data, output)
-	assert output.getvalue() == output_fastavro.getvalue()
+	output_cerializer = io.BytesIO()
+	serialize = cerializer.cerializer_handler.Cerializer(
+		['schemata']
+	).code[f'{schema_name}_{schema_version}']['serialize']
+	serialize(data, output_cerializer)
+	assert output_cerializer.getvalue() != io.BytesIO().getvalue()
+	assert output_cerializer.getvalue() == output_fastavro.getvalue()
